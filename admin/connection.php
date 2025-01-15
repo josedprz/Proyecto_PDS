be91cc9bc5860f8fa8ab100dd21458903fd66f38
Adrian-VL <adrianvl041003@gmail.com>
2025-01-15 13:22:24 -0600

    bd

--- admin/connection.php
+++ admin/connection.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');  // Usar solo 'localhost', el puerto se especifica en la configuración del servidor
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'pawfinder');
+define('DB_DATABASE', 'proyecto_pds');
 
 // Establecer la conexión
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
